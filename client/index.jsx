a955c6642c8f7406fad82d2e86dba3a41cff4909
Jordan Holmes <jordanstholmes@gmail.com>
2021-01-30 12:29:08 -0700

    Add basic React setup

---
+++ client/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div>Hello from react</div>, document.getElementById('app'));
+
+// console.log('the index file is bundled!');
