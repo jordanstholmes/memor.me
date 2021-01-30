54f76dfa72d838b719288caf431beefad215e029
Jordan Holmes <jordanstholmes@gmail.com>
2021-01-30 14:40:53 -0700

    Remove jest/enzyme setup test files

--- client/enzyme.test.jsx
+++
@@ -1,8 +0,0 @@
-// test file
-import React from 'react';
-import { shallow } from 'enzyme';
-
-const wrapper = shallow(<div>sample test</div>);
-test('enzyme works!', () => {
-  expect(wrapper.text()).toBe('sample test');
-});
